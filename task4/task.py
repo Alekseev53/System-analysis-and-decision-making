44885ad685c2608c3afab0bf5f3d06f69b3e9cea
Alekseev53 <alekseev.alex.romanovich@gmail.com>
2024-12-12 11:54:11 +0300

    update of 4

--- task4/task.py
+++ task4/task.py
@@ -4,23 +4,31 @@ def enthropy(probabilities):
     return -sum([prob * log2(prob) for prob in np.nditer(probabilities)])
 
 def main(input_matrix):
+    # Нормируем матрику
     initial_matrix = np.array(input_matrix)
     amount_of_purchases = initial_matrix.sum()
     compatible_event_prob_matrix = initial_matrix / amount_of_purchases
+    # Вычисляем вектор сумм по строчкам и по столбцам 
     p_y_vector = compatible_event_prob_matrix.sum(axis=1) 
     p_x_vector = compatible_event_prob_matrix.sum(axis=0) 
     
+    # Вычисляем энтропию
     H_XY = enthropy(compatible_event_prob_matrix)
     H_Y = enthropy(p_y_vector)
     H_X = enthropy(p_x_vector)
     conditional_prob_matrix = np.copy(compatible_event_prob_matrix)
     overall_conditional_H = 0
     
+    
     for row_index in range(len(compatible_event_prob_matrix)):
+        # Берем условную вероятность строки
         conditional_prob_matrix[row_index] /= p_y_vector[row_index]
+        # Берем энтропию строки
         conditional_H = enthropy(conditional_prob_matrix[row_index])    
+        # Добавляем в общую условную энтрапию
         overall_conditional_H += conditional_H * p_y_vector[row_index]
     
+    # Счиатем кол-во информации двумя методами
     information_quantity = H_X - overall_conditional_H
     H_XY_through_sum = H_Y + overall_conditional_H 
     
